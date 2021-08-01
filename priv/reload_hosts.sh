7fb7b2779495cc7abeaf9c6500f21b6f11e62d83
Ross <ross@flickswitch.co.za>
2021-08-01 22:08:31 +0200

    fix unit file and bash script

--- priv/reload_hosts.sh
+++ priv/reload_hosts.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/usr/bin/bash
 # Example shell script to automate hosts file updates
 # Add some commands to update your hosts files here
 # Then update Expunji
-./home/pi/expunji/_build/dev/rel/expunji/bin/expunji rpc "Expunji.Server.reload_hosts()"
+cd /home/pi/expunji/_build/prod/rel/expunji/bin
+./expunji rpc "Expunji.Server.reload_hosts()"
